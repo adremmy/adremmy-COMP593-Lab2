2c7d0ba741279b71084f3d19982e6c469ee99cd6
adremmy <156461031+adremmy@users.noreply.github.com>
2024-01-23 09:53:01 -0500

    first change
    before time ran out in lab

---
+++ lab2_script_template.py
@@ -0,0 +1,44 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+    about_me = {
+        'full_name': 'Remy Adepetun',
+        'student_id': 10327073,
+        'pizza_toppings': ['OLIVES', 'MUSHROOM', 'SAUSAGE'],
+        'movies': [
+         {'title': 'chuck', 'genre' :'comedy',
+         'title': 'you got served', 'genre' : 'dance drama'
+
+         }   
+        ],
+
+    }
+
+    # TODO: Step 3 - Add another movie to the data structure
+    another_movie = {
+        'title': 'stomp the yard', 'genre' : 'dance romance'
+    }
+    about_me['movies'].append(another_movie)
+# TODO: Step 4 - Function that prints student name and ID	
+    print(f'My name is {'full_name'}, but you can call me Mr {}')
+def print_student_name_and_id(about_me):
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
